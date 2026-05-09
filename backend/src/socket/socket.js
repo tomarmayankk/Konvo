71a805e7ed82217dcc4445feeedfca38c9dfbdde
tomarmayankk <mayanktomarpc@gmail.com>
2026-05-09 21:37:18 +0530

    changes for url

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -1,4 +1,5 @@
 import { Server } from "socket.io";
+
 import http from "http";
 
 import app from "../app.js";
@@ -7,7 +8,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: process.env.CLIENT_URL,
+    credentials: true,
   },
 });
 
@@ -19,31 +21,54 @@ const userSocketMap = {};
 
 
 // GET RECEIVER SOCKET ID
-export const getReceiverSocketId = (userId) => {
+
+export const getReceiverSocketId = (
+  userId
+) => {
   return userSocketMap[userId];
 };
 
 
-io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
+// SOCKET CONNECTION
 
-  const userId = socket.handshake.query.userId;
+io.on("connection", (socket) => {
+  console.log(
+    "A user connected",
+    socket.id
+  );
+
+  const userId =
+    socket.handshake.query.userId;
+
+  // STORE USER SOCKET
 
   if (userId) {
-    userSocketMap[userId] = socket.id;
+    userSocketMap[userId] =
+      socket.id;
   }
 
-  // SEND ONLINE USERS TO ALL CLIENTS
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  // SEND ONLINE USERS
+
+  io.emit(
+    "getOnlineUsers",
+    Object.keys(userSocketMap)
+  );
 
 
+  // DISCONNECT
 
   socket.on("disconnect", () => {
-    console.log("User disconnected", socket.id);
+    console.log(
+      "User disconnected",
+      socket.id
+    );
 
     delete userSocketMap[userId];
 
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    io.emit(
+      "getOnlineUsers",
+      Object.keys(userSocketMap)
+    );
   });
 });
 
--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -12,8 +12,8 @@ const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "lax",
-    secure: false,
+    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
   });
 
   return token;
